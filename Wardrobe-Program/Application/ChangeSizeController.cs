a2b0274b5ea1262f1ce270c638990ccb168358c3
Daniel Toft <daniel.toft96@gmail.com>
2021-11-16 20:01:32 +0100

    add ChangeSizeController + Maria wtf?!?!!?

---
+++ Wardrobe-Program/Application/ChangeSizeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Wardrobe_Program
+{
+	public class ChangeSizeController : IController
+	{
+		private readonly IDao<Garment> _garmentDao;
+
+		public ChangeSizeController(IDao<Garment> garmentDao)
+		{
+			_garmentDao = garmentDao;
+		}
+	}
+}
