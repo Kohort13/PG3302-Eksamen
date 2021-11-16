4fcfc84458ab8b9a0bfc570070030e61c308a83a
Daniel Toft <daniel.toft96@gmail.com>
2021-11-16 20:36:41 +0100

    add controllerTests 1 working 1 in progress

--- Wardrobe-Program/UnitTests/ControllersTest.cs
+++ Wardrobe-Program/UnitTests/ControllersTest.cs
@@ -20,6 +20,23 @@ namespace UnitTests
             app.Controllers["change-name"].Handle(new Command("change-name 0 A-new-name"));
             Assert.AreEqual("A-new-name", data.Retrieve(0).Name);
         }
-
+        [Test]
+        public void TestChangePriceController()
+        {
+            MockDatabase data = new();
+            data.Insert(new Accessory());
+            app.AddController("change-price", new ChangePriceController(data));
+            app.Controllers["change-price"].Handle(new Command("change-price 0 404"));
+            Assert.AreEqual("404", data.Retrieve(0).Price);
+        }
+        [Test]
+        public void TestChangeSizeController()
+        {
+	        MockDatabase data = new();
+	        data.Insert(new Accessory());
+	        app.AddController("change-size", new ChangeSizeController(data));
+	        app.Controllers["change-size"].Handle(new Command("change-size 0 new-size"));
+	        Assert.AreEqual("new-size", data.Retrieve(0).Size);
+        }
     }
 }
