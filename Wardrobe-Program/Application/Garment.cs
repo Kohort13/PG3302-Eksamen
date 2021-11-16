abf4e4e831975dbe3fc5f1e4512f1289bebed9f0
Maria Tan <mkvht@outlook.com>
2021-11-16 14:39:21 +0100

    Co-authored-by: MagnusHodne <MagnusHodne@users.noreply.github.com> Co-authored-by: Daniel Toft <Toftern@users.noreply.github.com>

--- Wardrobe-Program/Application/Garment.cs
+++ Wardrobe-Program/Application/Garment.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Wardrobe_Program
 {
@@ -6,9 +7,38 @@ namespace Wardrobe_Program
     {
         public string Name{get;set;}
         public int Price{get;set;}
-        public string Size{get;set;}
-        public List<Season> Seasons{get;set;}
+        public string Size{get;set;} = "onesize";
+        public List<string> Seasons{get;set;}
         public List<Material> Materials {get;set;}
         public string Notes{get;set;}
     }
+
+    #region GARMENT CONCRETE CLASSES
+
+    public class Shoe : Garment
+    {
+
+    }
+
+    public class Top : Garment
+    {
+        
+    }
+
+    public class Bottom : Garment
+    {
+        
+    }
+
+    public class Accessory : Garment
+    {
+        
+    }
+
+    public class Outerwear : Garment
+    {
+        
+    }
+
+    #endregion
 }
--- Wardrobe-Program/Application/GarmentFactory.cs
+++ Wardrobe-Program/Application/GarmentFactory.cs
@@ -2,8 +2,45 @@ using System;
 
 namespace Wardrobe_Program 
 {
-    public class GarmentFactory
+    public abstract class GarmentFactory
     {
+        public abstract Garment CreateGarment();
+    }
 
+    public class ShoeFactory : GarmentFactory
+    {
+        public Garment CreateGarment()
+        {
+            return new Shoe();
+        }
+    }
+
+    public class TopFactory : GarmentFactory
+    {
+        public Garment CreateGarment()
+        {
+            return new Top();
+        }
+    }
+    public class BottomFactory : GarmentFactory
+    {
+        public Garment CreateGarment()
+        {
+            return new BottomFactory();
+        }
+    }
+    public class AccessoryFactory : GarmentFactory
+    {
+        public Garment CreateGarment()
+        {
+            return new Accessory();
+        }
+    }
+    public class OuterwearFactory : GarmentFactory
+    {
+        public Garment CreateGarment()
+        {
+            return new Outerwear();
+        }
     }
 }
