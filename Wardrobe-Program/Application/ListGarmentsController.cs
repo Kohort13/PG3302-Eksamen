17d2d7de65f2de4df824e46a7841ef684acf2b72
Maria Tan <mkvht@outlook.com>
2021-11-17 15:07:15 +0100

    greating function ListGarmentsController (not complete)

---
+++ Wardrobe-Program/Application/ListGarmentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Wardrobe_Program
+{
+    class ListGarmentsController : IController
+    {
+        private readonly IDao<Garment> _availableGarments;
+
+        public ListGarmentsController(IDao<Garment> availableGarments)
+        {
+            _availableGarments = availableGarments;
+        }
+
+        public void Handle(Command command)
+        {
+            foreach (var commandComponent in _availableGarments.Keys)
+            {
+                UserInterface.Instance.Print($"commandComponent");
+            }
+        }
+    }
+}
