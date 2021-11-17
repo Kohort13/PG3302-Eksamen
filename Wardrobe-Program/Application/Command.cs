7e6c78ee817a169138d82980ea85eabb45e7b3da
Magnus Hodne <magnus@drobak.org>
2021-11-17 21:43:25 +0100

    Attempt at more advanced parsing of parameters

--- Wardrobe-Program/Application/Command.cs
+++ Wardrobe-Program/Application/Command.cs
@@ -8,18 +8,35 @@ namespace Wardrobe_Program
         public string Keyword { get; set; } = "";
         public Dictionary<string, string> Parameters { get; set; } = new();
 
-        //change-price -id 444 -v 256
+        //change-price -id 444 -v 256 -su -g
         public Command(string userInput) {
             var firstSpace = userInput.IndexOf(" ", StringComparison.Ordinal);
             Parameters = new Dictionary<string, string>();
             if (firstSpace > 0) {
                 var parametersAsList = new List<string>(userInput.Substring(firstSpace + 1).Split(" "));
-                // {"-id", "444", "-v", "256"}
-                for (int i = 0; i < parametersAsList.Count; i++) {
-                    if (parametersAsList[i].StartsWith('-')) {
-                        int nextElementIndex = i + 1 >= parametersAsList.Count ? i : i + 1;
-                        var value = !parametersAsList[nextElementIndex].StartsWith("-") ? parametersAsList[nextElementIndex] : "";
-                        Parameters.Add(parametersAsList[i], value);
+                //Når list-element starter på -, legg til ny key. Frem til du treffer en ny -, legg til påfølgende list-element til value for den key'en
+
+                //-id 1 -v green
+                int i = 0;
+                string key = "";
+                string value = "";
+
+                while (i < parametersAsList.Count) {
+                    var current = parametersAsList[i];
+
+                    if (i + 1 < parametersAsList.Count) {
+                        do {
+                            if (current.StartsWith('-')) {
+                                key = current;
+                                value = "";
+                                current = parametersAsList[i + 1 < parametersAsList.Count ? ++i : i];
+                            } else {
+                                value += current + " ";
+                                current = parametersAsList[i + 1 < parametersAsList.Count ? ++i : i];
+                            }
+                        } while (i + 1 <= parametersAsList.Count && !current.StartsWith('-'));
+
+                        Parameters.TryAdd(key, value.Trim());
                     }
                 }
             }
