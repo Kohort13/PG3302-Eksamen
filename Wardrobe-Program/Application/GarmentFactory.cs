99690b8993a1d34f36cd9e959ad6c3e890507a8c
Magnus Hodne <magnus@drobak.org>
2021-11-18 12:52:22 +0100

    Added Factory.GarmentType

--- Wardrobe-Program/Application/GarmentFactory.cs
+++ Wardrobe-Program/Application/GarmentFactory.cs
@@ -1,10 +1,9 @@
-using System;
-
 namespace Wardrobe_Program 
 {
     public abstract class GarmentFactory
     {
         public abstract Garment CreateGarment();
+        public abstract string GarmentType { get; }
     }
 
     #region ConcreteImplementations
@@ -14,6 +13,7 @@ namespace Wardrobe_Program
         public override Garment CreateGarment() {
             return new Shoe();
         }
+        public override string GarmentType => "Shoe";
     }
 
     public class TopFactory : GarmentFactory
@@ -21,24 +21,32 @@ namespace Wardrobe_Program
         public override Garment CreateGarment() {
             return new Top();
         }
+        public override string GarmentType => "Top";
+
     }
     public class BottomFactory : GarmentFactory
     {
         public override Garment CreateGarment() {
             return new Bottom();
         }
+        public override string GarmentType => "Bottom";
+
     }
     public class AccessoryFactory : GarmentFactory
     {
         public override Garment CreateGarment() {
             return new Accessory();
         }
+        public override string GarmentType => "Accessory";
+
     }
     public class OuterwearFactory : GarmentFactory
     {
         public override Garment CreateGarment() {
             return new Outerwear();
         }
+        public override string GarmentType => "Outerwear";
+
     }
 
     #endregion
