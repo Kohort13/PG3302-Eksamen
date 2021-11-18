49f150f6e1b4873b5a7d872bf83333b65e6ffe5a
Magnus Hodne <magnus@drobak.org>
2021-11-18 20:44:08 +0100

    ListGarments can do filtering

--- Wardrobe-Program/Application/Controllers/ListGarmentsController.cs
+++ Wardrobe-Program/Application/Controllers/ListGarmentsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Wardrobe_Program
 {
@@ -11,9 +12,56 @@ namespace Wardrobe_Program
         }
 
         public override void Handle(Command command) {
-            var garments = _availableGarments.ListAll();
-            foreach (var garment in garments) {
-                UserInterface.Instance.Print($"{garment}");
+            if (!ValidateCommand(command)) return;
+            if (command.Parameters.Keys.Count == 0) {
+                var garments = _availableGarments.ListAll();
+                foreach (var garment in garments)
+                {
+                    UserInterface.Instance.Print($"{garment}");
+                }
+            }
+            else {
+                List<Predicate<Garment>> matchers = new();
+                if (command.Parameters.ContainsKey("-type")) {
+                    matchers.Add(g => g.Subtype.Contains(command.Parameters["-type"]));
+                } if (command.Parameters.ContainsKey("-brand")) {
+                    matchers.Add(g => g.Brand.Contains(command.Parameters["-brand"]));
+                } if (command.Parameters.ContainsKey("-size")) {
+                    matchers.Add(g => g.Size.Contains(command.Parameters["-size"]));
+                } if (command.Parameters.ContainsKey("-note")) {
+                    matchers.Add(g => g.Note.Contains(command.Parameters["-note"]));
+                }
+                if (command.Parameters.ContainsKey("-price"))
+                {
+                    int firstDigitPos = command.Parameters["-price"].IndexOfAny(new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+                    string sign = command.Parameters["-price"].Substring(0, firstDigitPos);
+                    float number;
+                    try
+                    {
+                        number = Convert.ToSingle(command.Parameters["-price"].Substring(firstDigitPos));
+                    }
+                    catch (Exception)
+                    {
+                        UserInterface.Instance.Print("Invalid number");
+                        return;
+                    }
+                    if (sign.Contains(">")) {
+                        matchers.Add(g => g.Price > number);
+                    } else if (sign.Contains("<")) {
+                        matchers.Add(g => g.Price < number);
+                    }  else if (sign.Contains(">=")) {
+                        matchers.Add(g => g.Price >= number);
+                    } else if (sign.Contains("<=")) {
+                        matchers.Add(g => g.Price <= number);
+                    }
+                }
+
+
+                var garments = _availableGarments.ListSome(matchers.ToArray());
+                foreach (var garment in garments)
+                {
+                    UserInterface.Instance.Print($"{garment}");
+                }
             }
         }
 
@@ -21,6 +69,12 @@ namespace Wardrobe_Program
             return new ControllerValidator
             {
                 AvailableKeys = new()
+                {
+                    {"-type",(false, true)},
+                    {"-price",(false, true)},
+                    {"-size",(false, true)},
+                    {"-brand",(false, true)}
+                }
             };
         }
     }
